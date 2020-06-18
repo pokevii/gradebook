b55b27186ce00e49ab7545fbdfdc0276c3b850f7
pokevii <53920041+pokevii@users.noreply.github.com>
2020-06-17 22:13:47 -0700

    Add files via upload

---
+++ GradeBook/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IBook book = new DiskBook("Joshua's Grade Book");
+            book.GradeAdded += OnGradeAdded;
+
+            Console.WriteLine($"Please input grades into {book}. Type Q to stop.\n");
+
+            EnterGrades(book);
+
+            var stats = book.GetStats();
+
+            System.Console.WriteLine(InMemoryBook.CATEGORY);
+            System.Console.WriteLine($"For the book named {book.Name}");
+            System.Console.WriteLine($"Average: {(stats.Average):N1}");
+            System.Console.WriteLine($"Highest Grade: {(stats.HighestGrade):N1}");
+            System.Console.WriteLine($"Lowest Grade: {(stats.LowestGrade):N1}");
+            System.Console.WriteLine($"The letter grade is {stats.Letter}");
+        }
+
+        private static void EnterGrades(IBook book)
+        {            
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input == "Q")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        static void OnGradeAdded(object sender, EventArgs e)
+        {
+            System.Console.WriteLine("A grade was added.");
+        }
+    }
+}
---
+++ GradeBook/Statistics.cs
@@ -0,0 +1,60 @@
+using System;
+
+namespace GradeBook
+{
+    public class Statistics
+    {
+        public double Average
+        {
+            get
+            {
+                return Sum / Count;
+            }
+        }
+        public double HighestGrade;
+        public double LowestGrade; 
+        public char Letter
+        {
+            get
+            {
+                switch(Average)
+                {
+                    case var d when d >= 90.0:
+                        return 'A';
+                    
+                    case var d when d >= 80.0:
+                        return 'B';
+
+                    case var d when d >= 70.0:
+                        return 'C';
+
+                    case var d when d >= 60.0:
+                        return 'D';
+
+                    default:
+                        return 'F';
+                }
+            }
+        }
+        public double Sum;
+        public int Count;
+
+        public void Add(double number)
+        {
+            Sum += number;
+            Count += 1;
+            HighestGrade = Math.Max(number, LowestGrade);
+            LowestGrade = Math.Min(number, HighestGrade);   
+        }
+
+        public Statistics()
+        {
+            Count = 0;
+            Sum = 0.0;
+            HighestGrade = double.MinValue;
+            LowestGrade = double.MaxValue;
+        }
+    }
+    
+    
+}
